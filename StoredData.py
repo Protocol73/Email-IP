9b55511d81512f9da919a160042439aa4975a3fe
Protocol73 <mlaw4321@gmail.com>
2018-07-06 17:10:02 -0500

    Added run stop/check
    Added Check to stop file from being run directly.

--- StoredData.py
+++ StoredData.py
@@ -1,15 +1,34 @@
-#Stored Data.py
-#For use with IP-Email.py
-#Get at https://github.com/Protocol73
-
+#Stored Data.py For use with IP-Email.py
+#Get at https://github.com/Protocol73/Email-IP
+import os
+import time
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email.MIMEMultipart import MIMEMultipart
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+cls()  
+
+def importcheck():
+	cls()
+	print ("Do NOT run this file Directly.")
+	print ("Simply define your settings here,")
+	print ("And run Email-IP.py Your settings will be imported.")
+	time.sleep(10)
+	exit()
+
+if __name__ == '__main__':
+	importcheck()
+
+# ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ 
+# DO NOT Change anything above this Line.
+
+#Setting v v v 
 fromaddr = "example@gmail.com" #Your Gmail Address
 
 # v v v WARNING v v v
-password = "Password" #Gmail Password <--- WARNING THIS IS NOT SAFE! BE CAREFUL ! YOU WERE WARNED !
+password = "Password" #Gmail Password <--- WARNING THIS IS NOT SECURE! BE CAREFUL ! YOU WERE WARNED !
 # ^ ^ ^ WARNING ^ ^ ^
 
 to = "example@gmail.com" #Main recipient for this email.
@@ -27,3 +46,5 @@ var1 = raw_input('Varable Request 1:') #set these to what you want here & down b
 var2 = raw_input('Varable Request 2:') #set here for prompts & below for email text
 
 body1 = ("Installed: " + IP + "\r\nAfter ip Text \r\n" + "\r\nVarable Text 1:" + var1 + "\r\nVarable Text 2:" + var2)
+
+#Writen by Protocol73
